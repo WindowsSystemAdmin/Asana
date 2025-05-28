70e7600e94f6d04a2ff383024eb5a97935d20b66
Ethan Lin <elin@fsu.edu>
2025-05-28 00:17:13 -0400

    Inital pass of adding "Create" funcitonality for ToDo objects closes #1

---
+++ Asana.CLI/ToDo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asana.Library.Models
+{
+    public class ToDo
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int? Priority { get; set; }
+        public bool? IsCompleted { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Description}";
+        }
+    }
+}
