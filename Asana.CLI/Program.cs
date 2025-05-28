ca6e4214828242df7680a8bfe3fd097adf73045f
Ethan Lin <elin@fsu.edu>
2025-05-28 10:33:54 -0400

    Added Delete ToDo

--- Asana.CLI/Program.cs
+++ Asana.CLI/Program.cs
@@ -9,28 +9,35 @@ namespace Asana
         {
             var toDos = new List<ToDo>();
             int choiceInt;
+            var itemCount = 0;
             do
             {
                 Console.WriteLine("Choose a menu option:");
-                Console.WriteLine("1. Create a Todo");
-                Console.WriteLine("2. list all Todos");
-                Console.WriteLine("3. list all outstandingTodos");
-                Console.WriteLine("4. Exit");
+                Console.WriteLine("1. Create a ToDo");
+                Console.WriteLine("2. List all ToDos");
+                Console.WriteLine("3. List all outstanding ToDos");
+                Console.WriteLine("4. Delete a ToDo");
+                Console.WriteLine("5. Exit");
 
-                var choice = Console.ReadLine() ?? "4";
+                var choice = Console.ReadLine() ?? "5";
 
                 if (int.TryParse(choice, out choiceInt))
                 {
-
                     switch (choiceInt)
                     {
                         case 1:
-                            Console.WriteLine("Name:");
+                            Console.Write("Name:");
                             var name = Console.ReadLine();
-                            Console.WriteLine("Description:");
+                            Console.Write("Description:");
                             var description = Console.ReadLine();
 
-                            toDos.Add(new ToDo { Name = name, Description = description, IsCompleted = false });
+                            toDos.Add(new ToDo
+                            {
+                                Name = name,
+                                Description = description,
+                                IsCompleted = false,
+                                Id = ++itemCount
+                            });
                             break;
                         case 2:
                             toDos.ForEach(Console.WriteLine);
@@ -41,16 +48,30 @@ namespace Asana
                                 .ForEach(Console.WriteLine);
                             break;
                         case 4:
+                            toDos.ForEach(Console.WriteLine);
+                            Console.Write("ToDo to Delete: ");
+                            var toDoChoice = int.Parse(Console.ReadLine() ?? "0");
+
+                            var reference = toDos.FirstOrDefault(t => t.Id == toDoChoice);
+                            if (reference != null)
+                            {
+                                toDos.Remove(reference);
+                            }
+
+                            break;
+                        case 5:
                             break;
                         default:
-                            Console.WriteLine("ERROR:Unknown menu selection");
+                            Console.WriteLine("ERROR: Unknown menu selection");
                             break;
                     }
-                } else
-                {
-                    Console.WriteLine($"ERROR: {choice} is invalid menu selection");
                 }
-            } while (choiceInt != 4);
+                else
+                {
+                    Console.WriteLine($"ERROR: {choice} is not a valid menu selection");
+                }
+
+            } while (choiceInt != 5);
 
         }
     }
